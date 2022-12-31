291d65a634f9f974a8402581f307b68c2f6fd803
jurgden <82841924+jurgden@users.noreply.github.com>
2022-12-31 09:30:58 -0800

    Basic App Setup

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Homepage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to My Web App</h1>
+        <p>This is the homepage of my web app.</p>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
