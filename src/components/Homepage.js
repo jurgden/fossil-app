a3b67bc03b588f4f549a2e0d5491b98683b0f9a5
jurgden <82841924+jurgden@users.noreply.github.com>
2023-01-04 10:22:42 -0800

    anger and frustration

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "../css/homepage.css";
+import "../assets/paleo.png";
 
 class Homepage extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to My Web App</h1>
-        <p>This is the homepage of my web app.</p>
+      <div className="paleontology-theme">
+        <h1>Welcome to My Paleontology App</h1>
+        <p>This is the homepage of my paleontology app.</p>
+        <img src="../assets/paleo.png" alt="Fossil" />
       </div>
     );
   }
